2ed30ea349c7ff8e334ed281a63127295336554a
rajeevshete <rajeev.shete@cctech.co.in>
2025-11-07 11:19:38 +0530

    Added commented copy constructor and assignment operaotr

--- ConsoleApplication2/shape.cpp
+++ ConsoleApplication2/shape.cpp
@@ -18,3 +18,17 @@ bool shape::operator < (const shape& next) const
 {
 	return this->area() < next.area();
 }
+
+//shape::shape(const shape& a)		// copy constructor
+//{
+//	cout << "Copy constructor" << endl;
+//}
+
+//shape& shape::operator=(const shape& b)		//Assignment operator
+//{
+//	if (this != &b)
+//	{
+//		this->name = b.name;
+//	}
+//	return *this;
+//}
--- ConsoleApplication2/shape.h
+++ ConsoleApplication2/shape.h
@@ -13,6 +13,8 @@ public :
 	virtual ~shape();
 	string getname() const;
 	bool operator<(const shape& next) const;
+	/*shape(const shape& a);*/		// copy constructor
+	/*shape& operator=(const shape& b);*/		//Assignment operator
 	virtual double area() const = 0;
 	virtual double perimeter() const = 0;
 };
